191f32ee8e2828985abd7da4d50ae0f72857474f
Memku <mati30032007@gmail.com>
2026-01-05 20:27:42 +0100

    map now is centered on Poland upon opening the app

--- src/config/map.config.ts
+++ src/config/map.config.ts
@@ -4,7 +4,7 @@ export const MAP_CONFIG = {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 19,
   },
-  defaultCenter: [51.505, -0.09] as [number, number],
-  defaultZoom: 13,
+  defaultCenter: [52.0, 19.0] as [number, number],
+  defaultZoom: 6,
   markerIconSize: [40, 40] as [number, number],
 };
